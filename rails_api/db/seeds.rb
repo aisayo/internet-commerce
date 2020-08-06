8e753d68a906b3470c1b5e91c1fd620c770ae343
Aysan Isayo <aisayo@icloud.com>
2020-08-05 17:28:52 -0700

    created seed data for categories

--- rails_api/db/seeds.rb
+++ rails_api/db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create(name: 'Dresses')
+Category.create(name: 'Tops')
+Category.create(name: 'Shirts & Blouses')
+Category.create(name: 'Pantss')
+Category.create(name: 'Jeans')
+Category.create(name: 'Swimwear')
+Category.create(name: 'Basics')
+Category.create(name: 'Loungewear')
+Category.create(name: 'Shoes')
+Category.create(name: 'Sportswear')
+Category.create(name: 'Accessories')
+Category.create(name: 'Beauty')
+Category.create(name: 'Plus Sizes')
+
+
+
