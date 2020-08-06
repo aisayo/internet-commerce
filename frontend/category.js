eaa5bc9e168029777ba511ab1cec594097c54706
Aysan Isayo <aisayo@icloud.com>
2020-08-05 18:02:53 -0700

    added category link event handler

--- frontend/category.js
+++ frontend/category.js
@@ -1,7 +1,9 @@
 const BASE_URL = 'http://127.0.0.1:3000/';
+let navBar = document.getElementById('navbar')
 
 class Category {
-    constructor(name){
+    constructor(id, name){
+        this.id = id;
         this.name = name;
     }
 
@@ -10,20 +12,35 @@ class Category {
         .then(resp => resp.json())
         .then(categories =>{
             this.loadCategories(categories);
+            Category.addEventHandler();
         })
     }
 
     static loadCategories(categories){
         for (const category of categories){
-            let c = new Category(category.name);
+            let c = new Category(category.id, category.name);
             c.renderCategory();
         }
     }
     
     renderCategory(){
-        let navBar = document.getElementById('navbar')
-        let link = `<a href="#">${this.name}</a>`
-        navBar.innerHTML += link
+        let link = `<a class="category-link" data-id=${this.id} href="#">${this.name}</a>`;
+        navBar.innerHTML += link;
+        
+    }
+
+    
+
+    static addEventHandler(){
+        
+        navBar.addEventListener('click', Category.categoryEventHandler)
+    
+    }
+
+    static categoryEventHandler(){
+        if (event.target.matches('a.category-link')){
+            console.log('clicked!')
+        }
     }
 
 }
